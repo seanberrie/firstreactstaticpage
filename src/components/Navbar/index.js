876e23bcd4894842a9181599ef3cef5ee1a16033
Sean Berrie <seanberrie21@gmail.com>
2019-01-22 16:17:29 -0800

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Navbar.css'
+
+export default () => (
+  <nav className='navbar'>
+    <a href='#'>About</a>
+    <a href='#'>Gallery</a>
+    <a href='#'>Blog</a>
+    <a href='#'>Contact</a>
+  </nav>
+)
